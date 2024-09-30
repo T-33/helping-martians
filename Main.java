d6e5f08b04c1462eebf6d427b719619a2d1bc3ec
Timur <timurrahmatullaev703@gmail.com>
2024-09-30 18:32:29 +0600

    add users guess' verification

--- Main.java
+++ Main.java
@@ -5,14 +5,33 @@ class Main
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+
 		int[] cargoLocations = {3, 4, 1};
 		int[] guessedLocations = new int[3];
 
+		int correctGuessNumber = 0;
+		int attempts = 0;
+
 		for (int i = 0; i < cargoLocations.length; i += 1) {
+
 			guessedLocations[i] = sc.nextInt();
-			System.out.println(guessedLocations[i]);
+
 		}
 
 		sc.close();
+
+		for (int i = 0; i < cargoLocations.length; i += 1) {
+
+			for (int j = 0; j < cargoLocations.length; j += 1) {
+
+				if(guessedLocations[i] == cargoLocations[j]) correctGuessNumber += 1;
+
+			}
+
+		}
+
+		System.out.println(correctGuessNumber);
+
+	
 	}
 }
