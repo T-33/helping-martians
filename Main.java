584420ed3294ac7dbc97dd89910cf21ee1d2ab8b
Timur <timurrahmatullaev703@gmail.com>
2024-09-30 18:01:56 +0600

    add reading users' guesses from console

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		Scanner sc = new Scanner(System.in);
+		int[] cargoLocations = {3, 4, 1};
+		int[] guessedLocations = new int[3];
+
+		for (int i = 0; i < cargoLocations.length; i += 1) {
+			guessedLocations[i] = sc.nextInt();
+			System.out.println(guessedLocations[i]);
+		}
+
+		sc.close();
 	}
 }
