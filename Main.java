e7be7991abfb9775a3c404c2a449b244cc2452ab
Timur <timurrahmatullaev703@gmail.com>
2024-10-04 01:18:54 +0600

    add limited attempts

--- Main.java
+++ Main.java
@@ -1,37 +1,53 @@
 import java.util.Scanner;
 
-class Main
-{
+class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		int[] cargoLocations = {3, 4, 1};
+		int[] cargoLocations = { 3, 4, 1 };
 		int[] guessedLocations = new int[3];
 
 		int correctGuessNumber = 0;
 		int attempts = 0;
 
-		for (int i = 0; i < cargoLocations.length; i += 1) {
+		while (true) {
+			correctGuessNumber = 0;
 
-			guessedLocations[i] = sc.nextInt();
+			if(attempts == 5) {
+				System.out.println("Too many attempts, the cargos have changed their location . RERUN the programm");
+				break;
+			}
 
-		}
+			for (int i = 0; i < cargoLocations.length; i += 1) {
 
-		sc.close();
-
-		for (int i = 0; i < cargoLocations.length; i += 1) {
-
-			for (int j = 0; j < cargoLocations.length; j += 1) {
-
-				if(guessedLocations[i] == cargoLocations[j]) correctGuessNumber += 1;
+				guessedLocations[i] = sc.nextInt();
 
 			}
 
+			for (int i = 0; i < cargoLocations.length; i += 1) {
+
+				for (int j = 0; j < cargoLocations.length; j += 1) {
+
+					if (guessedLocations[i] == cargoLocations[j])
+
+						correctGuessNumber += 1;
+
+				}
+
+			}
+
+			if(correctGuessNumber == 3) {
+				System.out.println("You have correctly guessed all 3 locations");
+				break;
+			}
+
+			attempts += 1;
+
+			System.out.println(correctGuessNumber);
 		}
 
-		System.out.println(correctGuessNumber);
-
-	
+		sc.close();
+		
 	}
 }
